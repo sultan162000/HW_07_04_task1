12f0a94a68a69217e06b8ac684e1e76c7aaa24ae
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-08 20:13:40 +0500

    Создан первый экземпляр метода Shift()

--- ProgramApp/Program.cs
+++ ProgramApp/Program.cs
@@ -7,7 +7,13 @@ namespace ProgramApp
     {
         static void Main(string[] args)
         {
-            int[] a = {1,2,3,4,5,6};
+            int[] a = {1,2,3,4,5,6,7};
+            
+            Console.WriteLine(ArrayHelper.Shift(ref a));
+            foreach (var item in a)
+            {
+                System.Console.Write(item+" ");
+            }
             
 
         }
