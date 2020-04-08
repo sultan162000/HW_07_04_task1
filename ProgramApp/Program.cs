5004dda9f661484ff1848af0197dcc89a07043d7
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-08 20:39:58 +0500

    Закончил тестирование проекта. Запускать проект можно из папки ProgramApp файл Program.cs

--- ProgramApp/Program.cs
+++ ProgramApp/Program.cs
@@ -7,13 +7,38 @@ namespace ProgramApp
     {
         static void Main(string[] args)
         {
-            int[] a = {1,2,3,4,5,6};
-            
-            Console.WriteLine(ArrayHelper.UnShift(ref a,7));
+            int[] a = {7,2,3,4,5,6};
+
+            ArrayHelper.Shift(ref a);
             foreach (var item in a)
             {
                 System.Console.Write(item+" ");
             }
+            
+            Console.WriteLine(ArrayHelper.UnShift(ref a,1));
+            foreach (var item in a)
+            {
+                System.Console.Write(item+" ");
+            }
+            System.Console.WriteLine();
+
+
+
+            string[] userName = {"Google","Amazon","Пятёречка"};
+
+            Console.WriteLine(ArrayHelper.Pop(ref userName));
+            foreach (var item in userName)
+            {
+                System.Console.Write(item+" ");
+            }
+            System.Console.WriteLine();
+            ArrayHelper.Push(ref userName,"Microsoft");
+            foreach (var item in userName)
+            {
+                System.Console.Write(item+" ");
+            }
+
+
             
 
         }
